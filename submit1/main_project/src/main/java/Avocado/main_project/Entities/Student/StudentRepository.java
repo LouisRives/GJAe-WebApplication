15ecf131a13133fb3b03b32b534820d4e49d19dd
LouisRives <127422310+LouisRives@users.noreply.github.com>
2024-01-17 13:30:44 +0100

    Delete submit1 directory

--- submit1/main_project/src/main/java/Avocado/main_project/Entities/Student/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package Avocado.main_project.Entities.Student;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Set;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
--- submit1/main_project/src/main/java/Avocado/main_project/Entities/emailTemplate/EmailTemplateRepository.java
+++
@@ -1,8 +0,0 @@
-package Avocado.main_project.Entities.emailTemplate;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmailTemplateRepository extends JpaRepository<EmailTemplate, Long> {
-}
